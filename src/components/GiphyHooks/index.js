9b985f79ec319096a5d2d5202b2e7587daf590d5
thewalkingduff <62387646+thewalkingduff@users.noreply.github.com>
2021-06-11 22:05:05 -0400

    first commit

---
+++ src/components/GiphyHooks/index.js
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+
+const GifHooks = () => {
+    const [gifs, setGifs] = useState([])
+    const [savedGifs, setSavedGifs] = useState([])
+    const [gifInput, setGifInput] = useState('')
+
+
+    useEffect(() => {
+        const savedGifs = localStorage.getItem('savedGifs')
+
+        if (savedGifs) setSavedGifs(JSON.parse(savedGifs))
+    }, [])
+
+    const handleInput = (event) => {
+        setGifInput(event.target.value);
+    }
+
+    const handleRemoveGif = (index) => {
+        const newArray = [...savedGifs];
+
+        newArray.splice(index, 1);
+
+        setSavedGifs(newArray)
+        localStorage.setItem('savedGifs', JSON.stringify(newArray))
+
+    }
+
+    const handleSaveGif = (gif) => {
+        const newArray = [...savedGifs, gif]
+
+        setSavedGifs(newArray)
+        localStorage.setItem('savedGifs', JSON.stringify(newArray))
+    }
+
+    const handleSearchGifs = async () => {
+        if (!gifInput) return;
+
+        const res = await axios.get(`https://api.giphy.com/v1/gifs/search?&q=${gifInput}&api_key=xaJM8QTIIxH51RG17ImSyuh4xaxUc31N`)
+
+        console.log(res.data.data)
+
+        setGifs(res.data.data)
+    }
+
+    return (
+        <div>
+            <h1>My <span style={{ color: '#FFE62E' }}>Fav</span> GIPHYs</h1>
+
+            <div className='fav-gifs'>
+
+                {savedGifs.map((gif, index) => {
+                    console.log(gif)
+                    return (
+                        <div key={index} className="single-fav-gif">
+                            <img src={gif.images.fixed_width.url} alt=' ' />
+                            <button className="remove-button" onClick={() => handleRemoveGif(index)} >Remove</button>
+                        </div>
+                    )
+                })}
+            </div>
+
+            <section className='search-section'>
+                <h2>SEARCH GIPHYS HERE!</h2>
+                <input onChange={handleInput} value={gifInput} />
+                <button onClick={handleSearchGifs}>SEARCH</button>
+            </section>
+
+
+            <div className='searched-gifs'>
+                {gifs.map((gif, index) => {
+                    return (
+                        <div key={index} className="single-search-gif">
+                            <img src={gif.images.fixed_width.url} />
+                            <button onClick={() => handleSaveGif(gif)}>Save</button>
+                        </div>
+                    )
+                })}
+            </div>
+
+
+
+
+
+        </div>
+    );
+}
+
+export default GifHooks
